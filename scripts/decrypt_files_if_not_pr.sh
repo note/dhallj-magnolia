123dde784e8c2dc5a6fb3a35b4f8379457866fcd
Michal Sitko <pierwszy1@gmail.com>
2020-11-13 23:28:04 +0100

    Travis release

--- scripts/decrypt_files_if_not_pr.sh
+++ scripts/decrypt_files_if_not_pr.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 if [  [ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-  openssl aes-256-cbc -K $encrypted_3b9f0b9d36d1_key -iv $encrypted_3b9f0b9d36d1_iv -in secrets.tar.enc -out secrets.tar -d
+  openssl aes-256-cbc -K $encrypted_f7a2d53f3383_key -iv $encrypted_f7a2d53f3383_iv -in secrets.tar.enc -out secrets.tar -d
   tar xvf secrets.tar
 fi
