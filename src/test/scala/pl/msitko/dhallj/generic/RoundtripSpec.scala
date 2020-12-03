8881324f15ed7198428865f189f07227bd5335c0
Michal Sitko <pierwszy1@gmail.com>
2020-12-03 22:19:44 +0100

    Add RoundtripSpec

--- src/test/scala/pl/msitko/dhallj/generic/RoundtripSpec.scala
+++ src/test/scala/pl/msitko/dhallj/generic/RoundtripSpec.scala
@@ -2,17 +2,42 @@ package pl.msitko.dhallj.generic
 
 import org.dhallj.codec.{Decoder, Encoder}
 import org.dhallj.codec.syntax._
-import pl.msitko.dhallj.generic.example.AppConfig
-//import org.dhallj.codec.Encoder._
-import pl.msitko.dhallj.generic.GenericEncoder._
-import pl.msitko.dhallj.generic.GenericDecoder._
+import pl.msitko.dhallj.generic.example.{ApiConfig, AppConfig, DbConfig, EndpointConfig}
 
-//class RoundtripSpec extends munit.FunSuite with Fixtures {
-//  test("Should roundtrip") {
-//    val res = someAppConfig.asExpr
-//
-//  }
-//
-////  def roundtrip[T: Encoder: Decoder](in: T): T =
-////    in.asExpr.normalize().as[T].getOrElse(fail(s"roundtrip failed for $in"))
-//}
+abstract class RoundtripSpec extends munit.FunSuite with Fixtures {
+
+  def roundtrip[T: Encoder: Decoder](in: T): T =
+    in.asExpr.normalize().as[T].getOrElse(fail(s"roundtrip failed for $in"))
+}
+
+class AutoDeriveRoundtripSpec extends RoundtripSpec {
+  import pl.msitko.dhallj.generic.decoder.auto._
+  import pl.msitko.dhallj.generic.encoder.auto._
+
+  test("Should roundtrip") {
+    assertEquals(roundtrip(someAppConfig), someAppConfig)
+  }
+}
+
+class SemiautoDeriveRoundtripSpec extends RoundtripSpec {
+  import pl.msitko.dhallj.generic.decoder.semiauto._
+  import pl.msitko.dhallj.generic.encoder.semiauto._
+
+  implicit val appConfigDec = {
+    implicit val endpointConfigDec = deriveDecoder[EndpointConfig]
+    implicit val apiConfigDec      = deriveDecoder[ApiConfig]
+    implicit val dbConfigDec       = deriveDecoder[DbConfig]
+    deriveDecoder[AppConfig]
+  }
+
+  implicit val appConfigEnc = {
+    implicit val endpointConfigEnc = deriveEncoder[EndpointConfig]
+    implicit val apiConfigEnc      = deriveEncoder[ApiConfig]
+    implicit val dbConfigEnc       = deriveEncoder[DbConfig]
+    deriveEncoder[AppConfig]
+  }
+
+  test("Should roundtrip") {
+    assertEquals(roundtrip(someAppConfig), someAppConfig)
+  }
+}
