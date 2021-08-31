d58a00a1e92d945cd2062819e086f764953576b1
Scala Steward <me@scala-steward.org>
2021-08-31 11:14:46 +0200

    Update munit to 0.7.29

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 	object Versions {
 		val Dhall = "0.8.0-M1"
 		val Magnolia = "0.17.0"
-		val Munit = "0.7.27"
+		val Munit = "0.7.29"
 	}
 
 	def magnolia(scalaVersion: String) 	= Seq(
